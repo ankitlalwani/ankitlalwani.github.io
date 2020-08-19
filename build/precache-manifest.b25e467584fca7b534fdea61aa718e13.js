6242c27800737e8c38ce5eeb343fecfd1fb5deb7
Ankit Lalwani <36794216+ankitlalwani@users.noreply.github.com>
2020-08-18 23:21:00 -0400

    Portfolio

---
+++ build/precache-manifest.b25e467584fca7b534fdea61aa718e13.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cce200ec40d72b8e8adfedbb653d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d815e263329d74ca76",
+    "url": "/static/css/2.69f239f3.chunk.css"
+  },
+  {
+    "revision": "ad5ffa8e9b1d6ce89e14",
+    "url": "/static/css/main.bd112ec6.chunk.css"
+  },
+  {
+    "revision": "84d815e263329d74ca76",
+    "url": "/static/js/2.aede60c9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.aede60c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5ffa8e9b1d6ce89e14",
+    "url": "/static/js/main.088695cb.chunk.js"
+  },
+  {
+    "revision": "f438879c68bfc8864397",
+    "url": "/static/js/runtime-main.cc36b19e.js"
+  },
+  {
+    "revision": "7b694159aa275ba9ada6635c561da693",
+    "url": "/static/media/Contacts01.7b694159.png"
+  },
+  {
+    "revision": "b1ca73ff3458581b309fe9d4871cc940",
+    "url": "/static/media/Expense-01.b1ca73ff.png"
+  },
+  {
+    "revision": "9c1f8e1a1d550e8443594332292868ef",
+    "url": "/static/media/Tennis-Store01.9c1f8e1a.png"
+  },
+  {
+    "revision": "3de07e75fc30e1702ed142084462076f",
+    "url": "/static/media/University01.3de07e75.png"
+  },
+  {
+    "revision": "24e62cb4e9715d149e184773c54e8520",
+    "url": "/static/media/bg-image01.24e62cb4.png"
+  },
+  {
+    "revision": "322a35c141461470cccb3969ea1e196d",
+    "url": "/static/media/facebook01.322a35c1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8174398ff5a25d9ec911e9234f777abf",
+    "url": "/static/media/profile-pic.8174398f.png"
+  },
+  {
+    "revision": "d4a1f7817636aec86cc22e8e96f41740",
+    "url": "/static/media/profile-pic03.d4a1f781.png"
+  }
+]);
